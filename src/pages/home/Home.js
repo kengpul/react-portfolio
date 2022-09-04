01ee972c992b6d83ae28d43706ba074fd6f96e1f
kengpul <paulpulgares24@gmail.com>
2022-09-04 14:07:04 +0800

    index page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div id="intro" className="col-lg-10 d-flex flex-column justify-content-center align-items-center text-center">
+                    <h2 className="display-2">KING PAUL PULGARES</h2>
+                    <p className="lead sub">Aspiring Web Developer</p>
+                    <Link to='projects'>
+                        <button className="btn btn-dark w-100">VIEW PROJECTS</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
