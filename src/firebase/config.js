9037a20c4fb52353bc6f06d377f56d6785778565
kengpul <paulpulgares24@gmail.com>
2022-09-04 13:45:46 +0800

    firebase hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const { initializeApp } = 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrH0D4rQiptF_QfvnFrW7XX2deyn8hnfQ",
+    authDomain: "pulgareskingpaul.firebaseapp.com",
+    projectId: "pulgareskingpaul",
+    storageBucket: "pulgareskingpaul.appspot.com",
+    messagingSenderId: "666953684147",
+    appId: "1:666953684147:web:07e5a7915e93cabffaccc6"
+};
+
+const app = initializeApp(firebaseConfig);
