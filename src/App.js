335bbfb1a9c6d61583ec0947abb2956c9194b7f2
kengpul <paulpulgares24@gmail.com>
2022-09-04 14:10:55 +0800

    footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home';
 import Projects from './pages/projects/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='projects' element={<Projects />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div id="mainFooter">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-12 footer-items d-block d-lg-flex text-center">
+                        <p>&copy; 2022 King Paul Pulgares. All Rights Reserved.</p>
+                        <p>
+                            Email:
+                            <span><a href="mailto:paulpulgares24@gmail.com">paulpulgares24@gmail.com</a></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
