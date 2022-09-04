c43ea08fa70ff1a9dea446c31e4e402c1b3ae9b7
kengpul <paulpulgares24@gmail.com>
2022-09-04 18:03:48 +0800

    fix layout for index page in mobile view

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div className="container">
+        <div className="container home">
             <div className="row justify-content-center align-items-center">
                 <div id="intro" className="col-lg-10 d-flex flex-column justify-content-center align-items-center text-center">
                     <h2 className="display-2">KING PAUL PULGARES</h2>
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -70,6 +70,186 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
+                <div className="col-md-6">
+                    <div className="box">
+                        <img className="img-fluid img-preview"
+                            src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                            alt="onlineshop" />
+
+                        <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#onlineshop">
+                            <i className="fas fa-eye"></i> VIEW PROJECT
+                        </button>
+
+                        <div className="modal fade" id="onlineshop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-centered modal-lg">
+                                <div className="modal-content">
+                                    <div className="modal-body">
+                                        <div className="container-fluid">
+                                            <div className="row">
+                                                <div className="col-md-7 img-modal">
+                                                    <img
+                                                        src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                                                        className="img-fluid" alt="tip-calculator" />
+                                                </div>
+                                                <div className="col-md-5">
+                                                    <h3>Simple Online Shop</h3>
+                                                    <div className="modal-tags">
+                                                        <span>Bootstrap</span>
+                                                        <span>Javascript</span>
+                                                        <span>NodeJs</span>
+                                                        <span>MondgoDB</span>
+                                                    </div>
+
+                                                    <h5>About</h5>
+
+                                                    <div className="modal-about">
+                                                        <p>
+                                                            This is a simple online shop. it has a CRUD for cart. Add item to cart, modify the quantity
+                                                            of the
+                                                            item in the cart, remove item from the cart. It also have search bar to search a
+                                                            specific item.
+                                                        </p>
+                                                    </div>
+
+                                                    <a href="https://github.com/Kengpul/edamama" className="modal-btn">
+                                                        <i className="fas fa-eye"></i> DEMO SOON!
+                                                    </a>
+
+                                                    <a href="https://github.com/Kengpul" target=" _blank" className="modal-btn">
+                                                        <i className="fas fa-code"></i> CODE
+                                                    </a>
+
+                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="box">
+                        <img className="img-fluid img-preview"
+                            src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                            alt="onlineshop" />
+
+                        <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#onlineshop">
+                            <i className="fas fa-eye"></i> VIEW PROJECT
+                        </button>
+
+                        <div className="modal fade" id="onlineshop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-centered modal-lg">
+                                <div className="modal-content">
+                                    <div className="modal-body">
+                                        <div className="container-fluid">
+                                            <div className="row">
+                                                <div className="col-md-7 img-modal">
+                                                    <img
+                                                        src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                                                        className="img-fluid" alt="tip-calculator" />
+                                                </div>
+                                                <div className="col-md-5">
+                                                    <h3>Simple Online Shop</h3>
+                                                    <div className="modal-tags">
+                                                        <span>Bootstrap</span>
+                                                        <span>Javascript</span>
+                                                        <span>NodeJs</span>
+                                                        <span>MondgoDB</span>
+                                                    </div>
+
+                                                    <h5>About</h5>
+
+                                                    <div className="modal-about">
+                                                        <p>
+                                                            This is a simple online shop. it has a CRUD for cart. Add item to cart, modify the quantity
+                                                            of the
+                                                            item in the cart, remove item from the cart. It also have search bar to search a
+                                                            specific item.
+                                                        </p>
+                                                    </div>
+
+                                                    <a href="https://github.com/Kengpul/edamama" className="modal-btn">
+                                                        <i className="fas fa-eye"></i> DEMO SOON!
+                                                    </a>
+
+                                                    <a href="https://github.com/Kengpul" target=" _blank" className="modal-btn">
+                                                        <i className="fas fa-code"></i> CODE
+                                                    </a>
+
+                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="box">
+                        <img className="img-fluid img-preview"
+                            src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                            alt="onlineshop" />
+
+                        <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#onlineshop">
+                            <i className="fas fa-eye"></i> VIEW PROJECT
+                        </button>
+
+                        <div className="modal fade" id="onlineshop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-centered modal-lg">
+                                <div className="modal-content">
+                                    <div className="modal-body">
+                                        <div className="container-fluid">
+                                            <div className="row">
+                                                <div className="col-md-7 img-modal">
+                                                    <img
+                                                        src="https://camo.githubusercontent.com/7463ff87087ac7194933e6d1036b73c61f1730bdc84f0c9c31eb10f304a0851e/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f64736a7264726577642f696d6167652f75706c6f61642f76313635303936323730312f6564616d616d612f616c6c50726f64756374735f6f706a627a732e706e67"
+                                                        className="img-fluid" alt="tip-calculator" />
+                                                </div>
+                                                <div className="col-md-5">
+                                                    <h3>Simple Online Shop</h3>
+                                                    <div className="modal-tags">
+                                                        <span>Bootstrap</span>
+                                                        <span>Javascript</span>
+                                                        <span>NodeJs</span>
+                                                        <span>MondgoDB</span>
+                                                    </div>
+
+                                                    <h5>About</h5>
+
+                                                    <div className="modal-about">
+                                                        <p>
+                                                            This is a simple online shop. it has a CRUD for cart. Add item to cart, modify the quantity
+                                                            of the
+                                                            item in the cart, remove item from the cart. It also have search bar to search a
+                                                            specific item.
+                                                        </p>
+                                                    </div>
+
+                                                    <a href="https://github.com/Kengpul/edamama" className="modal-btn">
+                                                        <i className="fas fa-eye"></i> DEMO SOON!
+                                                    </a>
+
+                                                    <a href="https://github.com/Kengpul" target=" _blank" className="modal-btn">
+                                                        <i className="fas fa-code"></i> CODE
+                                                    </a>
+
+                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
