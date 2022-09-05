15e9013ad7206ee9015edf03561099e85f3c9f73
kengpul <paulpulgares24@gmail.com>
2022-09-05 16:17:26 +0800

    cleanup layouts

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,19 +4,19 @@ import './Links.css';
 export default function Links() {
     return (
         <div id="links">
-            <a type="button" id="linkedin" class="btn text-white" href="https://www.linkedin.com/in/kingpaulpulgares/"
+            <a type="button" id="linkedin" className="btn text-white" href="https://www.linkedin.com/in/kingpaulpulgares/"
                 target="_blank" rel="noreferrer">
                 LINKEDIN<FaLinkedin className='ms-3' />
             </a>
-            <a type="button" id="github" class="btn text-white" href="https://github.com/Kengpul"
+            <a type="button" id="github" className="btn text-white" href="https://github.com/Kengpul"
                 target="_blank" rel="noreferrer">
                 &nbsp;&nbsp;&nbsp;GITHUB <FaGithub className='ms-3' />
             </a>
-            <a type="button" id="email" class="btn text-white" href="mailto:paulpulgares24#gmail.com"
+            <a type="button" id="email" className="btn text-white" href="mailto:paulpulgares24#gmail.com"
                 target="_blank" rel="noreferrer">
                 &nbsp;&nbsp;&nbsp;&nbsp; EMAIL <FaEnvelope className='ms-3' />
             </a>
-            <a type="button" id="resume" class="btn text-white"
+            <a type="button" id="resume" className="btn text-white"
                 href="https://drive.google.com/file/d/1v6cheSRU18nMKBW0Um_32eHDKTEGW6_e/view"
                 target="_blank" rel="noreferrer">
                 RESUME <FaIdBadge className='ms-3' />
