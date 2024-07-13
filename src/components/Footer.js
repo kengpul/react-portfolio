2540953872696cf2c412e3649c9be4e076623c20
kengpul <paulpulgares24@gmail.com>
2024-07-13 16:29:03 +0800

    fix/homepage layout and update links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 footer-items d-block d-lg-flex text-center">
-                        <p>&copy; 2023 King Paul Pulgares. All Rights Reserved.</p>
+                        <p>&copy; 2024 King Paul Pulgares. All Rights Reserved.</p>
                         <p>
                             Email:
                             <span><a href="mailto:paulpulgares24@gmail.com">paulpulgares24@gmail.com</a></span>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="row justify-content-center align-items-center">
                 <div id="intro" className="col-lg-10 d-flex flex-column justify-content-center align-items-center text-center">
                     <h2 className="display-2">KING PAUL PULGARES</h2>
-                    <p className="lead sub">Aspiring Software Developer</p>
+                    <p className="lead sub">Software Engineer</p>
                     <Link to='projects'>
                         <button className="btn btn-dark w-100">VIEW PROJECTS</button>
                     </Link>
